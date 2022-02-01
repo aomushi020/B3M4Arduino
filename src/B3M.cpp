ece2f809844b7d280f44982b427a500309f84495
aomushi020 <aomushi020@users.noreply.github.com>
2022-02-02 00:58:27 +0900

    version fix, add license info

---
+++ src/B3M.cpp
@@ -0,0 +1,6 @@
+/*
+    B3M4Arduino
+    Copyright (c) 2022 aomushi020
+    https://github.com/aomushi020/B3M4Arduino
+    This library is released under the MIT license.
+*/
---
+++ src/B3M.h
@@ -0,0 +1,13 @@
+/*
+    B3M4Arduino
+    Copyright (c) 2022 aomushi020
+    https://github.com/aomushi020/B3M4Arduino
+    This library is released under the MIT license.
+*/
+
+#ifndef B3M_h
+#define B3M_h
+
+
+
+#endif
