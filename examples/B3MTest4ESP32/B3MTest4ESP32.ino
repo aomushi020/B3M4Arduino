9baeafc3f6e1bf09a1304529e01129a47c7e9774
aomushi020 <aomushi020@users.noreply.github.com>
2022-02-03 12:53:22 +0900

    fix esp32 sample

--- examples/B3MTest4ESP32/B3MTest4ESP32.ino
+++ examples/B3MTest4ESP32/B3MTest4ESP32.ino
@@ -4,14 +4,23 @@
     https://github.com/aomushi020/B3M4Arduino
     This library is released under the MIT license.
 */
-#include <B3M.h>
 
-B3M B3M($Serial2, 16, 17, 18, 115200, 100);
+#include <B3M.h>
+HardwareSerial RS485(2);
+B3M B3M(&RS485, 16, 17, 18, 115200, 1000);
 
 void setup(void){
     B3M.begin();
-    B3M.reset(0xFF, 0x80, 0x00);
-    
+    Serial.begin(115200);
+    B3M.reset(0xFF, 0x00, 0x00);
+    uint8_t sendData[2] = {0x00, 0x00};
+    Serial.println(B3M.write(0xFF, 0x00, sendData, 0x01, 0x28), DEC);
+    delay(1000);
+    uint8_t sendData2[2] = {0x00, 0x00};
+    Serial.println(B3M.write(0xFF, 0x00, sendData2, 0x01, 0x29), DEC);
+    delay(1000);
+    Serial.println(B3M.position(0xFF, 0x00, 0x0000, 0x0000), DEC);
+
 }
 
 void loop(void){
