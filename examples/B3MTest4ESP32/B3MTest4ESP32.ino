d1b3c77000d0fdc6e37bf5119fef8afcbc1f93fb
aomushi020 <aomushi020@users.noreply.github.com>
2022-02-03 14:29:38 +0900

    fix sample code

--- examples/B3MTest4ESP32/B3MTest4ESP32.ino
+++ examples/B3MTest4ESP32/B3MTest4ESP32.ino
@@ -10,16 +10,15 @@ HardwareSerial RS485(2);
 B3M B3M(&RS485, 16, 17, 18, 115200, 1000);
 
 void setup(void){
-    B3M.begin();
     Serial.begin(115200);
-    B3M.reset(0xFF, 0x00, 0x00);
+    B3M.begin();
+    B3M.reset();
     uint8_t sendData[2] = {0x00, 0x00};
-    Serial.println(B3M.write(0xFF, 0x00, sendData, 0x01, 0x28), DEC);
+    B3M.write(0xFF, sendData, 0x01, 0x28);
     delay(1000);
-    uint8_t sendData2[2] = {0x00, 0x00};
-    Serial.println(B3M.write(0xFF, 0x00, sendData2, 0x01, 0x29), DEC);
+    B3M.write(0xFF, sendData, 0x01, 0x29);
     delay(1000);
-    Serial.println(B3M.position(0xFF, 0x00, 0x0000, 0x0000), DEC);
+    B3M.position(0xFF, 0x0000);
 
 }
 
