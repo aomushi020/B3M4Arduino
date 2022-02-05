2bbd218f7ceda466a2f53c8797b0421aee0483fe
aomushi020 <aomushi020@users.noreply.github.com>
2022-02-05 12:23:49 +0900

    add samples

--- examples/B3MTest4Uno/B3MTest4Uno.ino
+++ examples/B3MTest4Uno/B3MTest4Uno.ino
@@ -4,3 +4,28 @@
     https://github.com/aomushi020/B3M4Arduino
     This library is released under the MIT license.
 */
+
+#include <B3M.h>
+B3M B3M(&Serial, 16, 115200, 1000);
+
+void setup(void){
+    B3M.begin();
+    B3M.reset();
+    uint8_t sendData[2] = {0x02, 0x00};
+    // Free
+    B3M.write(B3M_BROADCAST_ADDR, sendData, 0x01, B3M_SERVO_MODE_SET);
+    delay(1000);
+    sendData[0] = 0x00;
+    // Set Even
+    B3M.write(B3M_BROADCAST_ADDR, sendData, 0x01, B3M_SERVO_CONTROL_TYPE);
+    delay(1000);
+    // Start control
+    B3M.write(B3M_BROADCAST_ADDR, sendData, 0x01, B3M_SERVO_MODE_SET);
+    delay(1000);
+    // set position 0
+    B3M.position(B3M_BROADCAST_ADDR, 0x0000);
+}
+
+void loop(void){
+
+}
